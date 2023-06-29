789e45d41090c2db5c97586fc9c3153c32b07dab
TarcisioH <42574172+TarcisioH@users.noreply.github.com>
2023-06-29 10:35:32 -0300

    Publicando o jogo

---
+++ js/validacao.js
@@ -0,0 +1,35 @@
+function verificarChute(chute) {
+    const numero = +chute;
+
+    if (Number.isNaN(numero)){
+        elementoChute.innerHTML += '<div>Valor inválido</div>';
+        return
+    }
+
+    if (numero > maiorValor || numero < menorValor){
+        elementoChute.innerHTML += ` <div>Valor inválido, o número precisa estar no intervalo de ${menorValor} a ${maiorValor} </div>`;
+        return
+    }
+
+    if (numero === numeroSecreto) {
+        document.body.innerHTML = `
+        <h2>Você acertou!</h2>
+        <h3>O número secreto era ${numeroSecreto}</h3>
+        <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
+        `
+    } else if (numero > numeroSecreto) {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-arrow-down"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+        <div>O número secreto é maior <i class="fa-solid fa-arrow-up"></i></div>
+        `
+    }
+}
+
+document.body.addEventListener('click', (e) => {
+    if (e.target.id == 'jogar-novamente') {
+        window.location.reload();
+    }
+})
